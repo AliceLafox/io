4521bc3368decbe1f0a76994b7733af1648ac315
Alice Lafox <alice@lafox.net>
2015-12-19 22:54:58 +0200

    Init

---
+++ src/main/java/net/lafox/io/controller/ImageRestController.java
@@ -0,0 +1,23 @@
+package net.lafox.io.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("api")
+public class ImageRestController {
+
+    @RequestMapping(value = "test")
+    public Object test(HttpServletRequest request) {
+        Map<String, Object> map=new HashMap<>();
+        map.put("1", "1");
+        return map;
+
+    }
+
+
+}
